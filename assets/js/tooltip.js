19e008bdd129c48390a0dae702ce61776390feb8
Tin Nguyen <tin332810@gmail.com>
2024-09-30 09:08:03 +0700

    Finished product page - UI

---
+++ assets/js/tooltip.js
@@ -0,0 +1,30 @@
+function showTooltip(event, text) {
+    const tooltip = document.getElementById('tooltip');
+    const tooltipText = document.getElementById('tooltip-text');
+    tooltip.style.display = 'block';
+
+    const tooltipWidth = tooltip.offsetWidth;
+    const tooltipHeight = tooltip.offsetHeight;
+    let left = event.pageX;
+    let top = event.pageY; 
+    
+    tooltipText.textContent = text;
+
+    if (left + tooltipWidth > window.innerWidth) {
+        left = event.pageX - tooltipWidth;
+    }
+
+    if (top + tooltipHeight > window.innerHeight) {
+        top = event.pageY - tooltipHeight;
+    }
+
+    tooltip.style.left = `${left}px`;
+    tooltip.style.top = `${top}px`;
+    tooltip.classList.remove('hidden');
+    tooltip.style.display = '';
+}
+
+function hideTooltip() {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.classList.add('hidden');
+}
