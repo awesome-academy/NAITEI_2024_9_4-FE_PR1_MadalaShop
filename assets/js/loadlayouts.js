129d14dc3231a61b4287d33cc02b5657615205dc
Tinhvila <ptinh4225@gmail.com>
2024-10-01 13:03:44 +0700

    Finish cart UI + responsive

--- assets/js/loadlayouts.js
+++ assets/js/loadlayouts.js
@@ -9,11 +9,10 @@ function loadLayout(elementId, filePath, contentId = null) {
     .then((data) => {
       const tempDiv = document.createElement('div');
       tempDiv.innerHTML = data;
-      
+
       let content;
       if (contentId) {
         content = tempDiv.querySelector(`#${contentId}`);
-        if (!content) throw new Error(`Không tìm thấy phần tử với id ${contentId} trong ${filePath}`);
       } else {
         content = tempDiv;
       }
